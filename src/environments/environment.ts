64d8d556bf19e6c859c4637a239fe1310866ffda
vineetsah <vineetsah345@gmail.com>
2019-08-05 23:28:03 +0530

    fix issue 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  api_url: 'http://185.172.165.146:3334/api',
-  asset_url: 'http://185.172.165.146:3334/api/'
+  api_url: 'http://13.234.60.25:3334/api',
+  asset_url: 'http://13.234.60.25:3334/',
+
 };
