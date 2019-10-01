d37a4fff6b00157a446da2b79d2bf0499c338ae7
Ankit <ankitphondani52@gmail.com>
2019-10-01 17:31:39 +0530

    Resolved conflict

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // api_url: 'http://185.172.165.146/api',
-  // asset_url: 'http://185.172.165.146/',
+  // api_url: 'http://185.172.165.146:3334/api',
+  // asset_url: 'http://185.172.165.146:3334/',
   api_url: 'http://13.234.60.25:3334/api',
   asset_url: 'http://13.234.60.25:3334/'
 };
