81797176e15b0447ef65c4fc8402da60edc3ca46
Ankit <ankitphondani52@gmail.com>
2020-03-05 00:54:49 +0530

    Fixed password reset flow

--- src/app/account/resetpassword/resetpassword.component.ts
+++ src/app/account/resetpassword/resetpassword.component.ts
@@ -3,53 +3,53 @@ import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
 import { ApiService, JwtService } from '../../core/services';
 
 @Component({
-	selector: 'app-resetpassword-page',
-	templateUrl: './resetpassword.component.html',
-	styleUrls: ['./resetpassword.component.css']
+  selector: 'app-resetpassword-page',
+  templateUrl: './resetpassword.component.html',
+  styleUrls: ['./resetpassword.component.css']
 })
 
 export class ResetpasswordComponent implements OnInit {
 
-	public resetObj: any;
-	public validation: any;
+  public resetObj: any;
+  public validation: any;
 
 
-	constructor(private router: Router, public route: ActivatedRoute, public api: ApiService, public jwt: JwtService) {
+  constructor (private router: Router, public route: ActivatedRoute, public api: ApiService, public jwt: JwtService) {
 
-		this.resetObj = {
-			id: '',
-			password: '',
-			newpassword: '',
-			password_reset_token: ''
-		};
-	}
+    this.resetObj = {
+      id: '',
+      password: '',
+      newpassword: '',
+      password_reset_token: ''
+    };
+  }
 
-	resetPassword() {
+  ngOnInit() {
+    this.resetObj.userId = this.route.snapshot.paramMap.get('id');
+    this.resetObj.password_reset_token = localStorage.getItem('password_reset');
+  }
 
-		if (this.resetObj.password == "" || this.resetObj.newpassword == "") {
-			this.validation = "Require fields are empty";
-		}
-		else if (this.resetObj.password !== this.resetObj.newpassword) {
-			this.validation = "Password does not match";
-		}
-		else {
-			this.api.post("/auth/password-reset", this.resetObj)
-				.subscribe((response) => {
-					if (response.status == 200) {
-						this.validation = "";
-						localStorage.removeItem('password_reset');
-						this.router.navigate(['/account/login']);
-					}
-				}, (err) => {
-					if (err.message !== undefined) {
-						this.validation = err.message
-					}
-				})
-		}
-	}
+  resetPassword() {
 
-	ngOnInit() {
-		this.resetObj.userId = this.route.snapshot.paramMap.get('id');
-		this.resetObj.password_reset_token = localStorage.getItem('password_reset');
-	}
+    if (this.resetObj.password == "" || this.resetObj.newpassword == "") {
+      this.validation = "Require fields are empty";
+    }
+    else if (this.resetObj.password !== this.resetObj.newpassword) {
+      this.validation = "Password does not match";
+    }
+    else {
+      this.api.post("/auth/password-reset", this.resetObj)
+        .subscribe((response) => {
+          if (response.status == 200) {
+            this.validation = "";
+            localStorage.removeItem('password_reset');
+            this.router.navigate(['/account/login']);
+          }
+        }, (err) => {
+          if (err.message !== undefined) {
+            this.validation = err.message
+          }
+        })
+    }
+  }
 }
--- src/app/account/verifyotp/verifyotp.component.ts
+++ src/app/account/verifyotp/verifyotp.component.ts
@@ -3,48 +3,49 @@ import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
 import { ApiService, JwtService } from '../../core/services';
 
 @Component({
-	selector: 'app-verifyotp-page',
-	templateUrl: './verifyotp.component.html',
-	styleUrls: ['./verifyotp.component.css']
+  selector: 'app-verifyotp-page',
+  templateUrl: './verifyotp.component.html',
+  styleUrls: ['./verifyotp.component.css']
 })
 
 export class VerifyotpComponent implements OnInit {
 
-	public otpObj: any;
-	public validation: any;
+  public otpObj: any;
+  public validation: any;
 
-	constructor(private router: Router, public route: ActivatedRoute, public api: ApiService, public jwt: JwtService) {
+  constructor (private router: Router, public route: ActivatedRoute, public api: ApiService, public jwt: JwtService) {
 
-		this.otpObj = {
-			otp: '',
-			userId: '',
-		}
-	}
+    this.otpObj = {
+      otp: '',
+      userId: '',
+    }
+  }
 
-	verifyOtp() {
+  verifyOtp() {
 
-		if (this.otpObj.otp == "") {
-			this.validation = "Require fields are empty";
-		} else {
+    if (this.otpObj.otp == "") {
+      this.validation = "Require fields are empty";
+    } else {
 
-			this.api.post("/auth/otp-verify", this.otpObj)
-				.subscribe((response) => {
-					console.log(response);
-					if (response.status == 200) {
-						this.router.navigate(['/account/resetpassword/' + response.data._id]);
-					}
-				}, (err) => {
-					if (err.message !== undefined) {
-						this.validation = err.message
-					}
-				})
-		}
-	}
+      this.api.post("/auth/otp-verify", this.otpObj)
+        .subscribe((response) => {
+          console.log(response);
+          if (response.status == 200) {
+            localStorage.setItem('password_reset', response.data.password_reset_token);
+            this.router.navigate(['/account/resetpassword/' + response.data._id]);
+          }
+        }, (err) => {
+          if (err.message !== undefined) {
+            this.validation = err.message
+          }
+        })
+    }
+  }
 
-	ngOnInit() {
-		this.otpObj.userId = this.route.snapshot.paramMap.get('id');
-	}
-	resentotp() {
+  ngOnInit() {
+    this.otpObj.userId = this.route.snapshot.paramMap.get('id');
+  }
+  resentotp() {
 
-	}
+  }
 }
